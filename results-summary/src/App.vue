a7dadf1c0090675ee8f6c0cea57ac3645fd8e3f7
Lorena <lorenadomingos@proton.me>
2026-04-13 20:03:37 -0300

    Add icon-memory.svg, icon-reaction.svg, icon-verbal.svg, and icon-visual.svg to public/assets/images for categorization icons

--- results-summary/src/App.vue
+++ results-summary/src/App.vue
@@ -1,11 +1,50 @@
-<script setup></script>
+<script setup>
+import content from '@/data/data.json'
+import './style.css'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <div class="container">
+      <div class="card-one">
+        <p>Your Result</p>
+        <h1>76
+          <br><span class="card-one-span">of 100</span>
+        </h1>
+        <h2>Great</h2>
+        <p>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </div>
+
+      <div class="card-two">
+        <p>Summary</p>
+        <div v-for="data in content" :key="data.category" class="card-list">
+          <h3>{{ data.category }}</h3>
+          <span>{{ data.score }}</span>
+          <img :src="data.icon" alt="">
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+}
+
+.card-one {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 18rem;
+  text-align: center;
+  gap: 1rem;
+}
+
+.card-one-span {
+  font-size: 1rem;
+  font-weight: 400;
+}
+</style>
